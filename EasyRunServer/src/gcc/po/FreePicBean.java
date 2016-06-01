ebf1a0a34911f71f4ad2c9dba780a5ac149077e6
gecongcong <congcong.ge@foxmail.com>
2016-06-02 00:07:47 +0800

    1

---
+++ EasyRunServer/src/gcc/po/FreePicBean.java
@@ -0,0 +1,90 @@
+package gcc.po;
+
+public class FreePicBean
+{
+	private int eventID = -1;
+	private String wechatID = "";
+	private String picID = "";
+	private int downloadCnt = -1;
+	private long upTime = -1;
+	private String userName = "";
+	private String eventName = "";
+	private String headImgUrl =""; // 用户头像链接
+	
+	public int getEventID()
+	{
+		return eventID;
+	}
+
+	public void setEventID(int eventID)
+	{
+		this.eventID = eventID;
+	}
+
+	public String getWechatID()
+	{
+		return wechatID;
+	}
+
+	public void setWechatID(String wechatID)
+	{
+		this.wechatID = wechatID;
+	}
+
+	public String getPicID()
+	{
+		return picID;
+	}
+
+	public void setPicID(String picID)
+	{
+		this.picID = picID;
+	}
+
+	public int getDownloadCnt()
+	{
+		return downloadCnt;
+	}
+
+	public void setDownloadCnt(int downloadCnt)
+	{
+		this.downloadCnt = downloadCnt;
+	}
+
+	public long getUpTime()
+	{
+		return upTime;
+	}
+
+	public void setUpTime(long upTime)
+	{
+		this.upTime = upTime;
+	}
+
+	public String getEventName() {
+		return eventName;
+	}
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getHeadImgUrl() {
+		return headImgUrl;
+	}
+
+	public void setHeadImgUrl(String headImgUrl) {
+		this.headImgUrl = headImgUrl;
+	}
+
+	
+	
+}
---
+++ EasyRunServer/src/gcc/po/UserBean.java
@@ -0,0 +1,152 @@
+package gcc.po;
+
+import java.sql.Date;
+
+public class UserBean
+{
+	public static enum Gender
+	{
+		male,female;
+	}
+	private String userID = "";
+	private String headImgUrl =""; 	// 用户头像链接
+	private String userName = "";	//昵称
+	private String account = "";	//用户名
+	private String password = "";	//密码
+	private String realName = "";	//真实姓名
+	private String celphone = "";
+	private String email = "";
+	private Date birth;
+	private Gender gender;
+	private String identityCard = "";	//身份证号
+	private String identityPic = "";	//身份证上传图的存储路径
+	private String bloodType = "";		//血型
+	private String address = "";		//住址：省+市+区/县
+	private float height;				//身高
+	private float weight;				//体重
+	private String urgencyContact = ""; //紧急联系人姓名
+	private String urgencyPhone = "";	//紧急联系人电话
+	
+	
+	public String getEmail()
+	{
+		return email;
+	}
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+	public Date getBirth()
+	{
+		return birth;
+	}
+	public void setBirth(Date birth)
+	{
+		this.birth = birth;
+	}
+	public Gender getGender()
+	{
+		return gender;
+	}
+	public void setGender(int gender)
+	{
+		this.gender = Gender.values()[gender];
+	}
+	public String getUserName()
+	{
+		return userName;
+	}
+	public void setUserName(String userName)
+	{
+		this.userName = userName;
+	}
+	public String getCelphone()
+	{
+		return celphone;
+	}
+	public void setCelphone(String celphone)
+	{
+		this.celphone = celphone;
+	}
+	public String getIdentityCard() {
+		return identityCard;
+	}
+	public void setIdentityCard(String identityCard) {
+		this.identityCard = identityCard;
+	}
+	public String getIdentityPic() {
+		return identityPic;
+	}
+	public void setIdentityPic(String identityPic) {
+		this.identityPic = identityPic;
+	}
+	public String getBloodType() {
+		return bloodType;
+	}
+	public void setBloodType(String bloodType) {
+		this.bloodType = bloodType;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public float getHeight() {
+		return height;
+	}
+	public void setHeight(float height) {
+		this.height = height;
+	}
+	public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	public String getUrgencyContact() {
+		return urgencyContact;
+	}
+	public void setUrgencyContact(String urgencyContact) {
+		this.urgencyContact = urgencyContact;
+	}
+	public String getUrgencyPhone() {
+		return urgencyPhone;
+	}
+	public void setUrgencyPhone(String urgencyPhone) {
+		this.urgencyPhone = urgencyPhone;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	public String getHeadImgUrl() {
+		return headImgUrl;
+	}
+	public void setHeadImgUrl(String headImgUrl) {
+		this.headImgUrl = headImgUrl;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getRealName() {
+		return realName;
+	}
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+}
